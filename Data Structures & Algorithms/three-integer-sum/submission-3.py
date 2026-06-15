87661df42425be1688652d7ee453288b931c33b8
ParthIll <146685408+ParthIll@users.noreply.github.com>
2026-06-15 13:30:49 -0500

    Add: three-integer-sum - submission-3

---
+++ Data Structures & Algorithms/three-integer-sum/submission-3.py
@@ -0,0 +1,31 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        numbers.sort()
+        i=0
+        j=len(numbers)-1
+        while(i<j):
+            numSum = numbers[i]+numbers[j]
+            if numSum<target:
+                i+=1
+            elif numSum>target:
+                j-=1
+            else:
+                return [target*-1,numbers[i],numbers[j]]
+        return []
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ret = {}
+        for i in range(len(nums)):
+            if i==0:
+                newNums = nums[1:]
+            elif i==len(nums)-1:
+                newNums = nums[0:i]
+            else:
+                newNums = nums[:i] + nums[i+1:]  
+            print(self.twoSum(newNums,nums[i]*-1))
+            while(self.twoSum(newNums,nums[i]*-1)!=[]):
+                ret[tuple(sorted(self.twoSum(newNums,nums[i]*-1)))]=1
+                Leest = self.twoSum(newNums,nums[i]*-1)
+                newNums.remove(Leest[1])
+                newNums.remove(Leest[2])
+        return list(ret)
+        
