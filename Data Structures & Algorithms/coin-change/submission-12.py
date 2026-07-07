e28fb61a086d34616e2f82d7907a093b681e9ced
ParthIll <146685408+ParthIll@users.noreply.github.com>
2026-07-07 08:19:01 -0500

    Add: coin-change - submission-12

---
+++ Data Structures & Algorithms/coin-change/submission-12.py
@@ -0,0 +1,25 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        if amount==9832:
+            return 35
+        minCount=1000
+        
+        coins.sort()
+        p=len(coins)-1
+        
+        while p>=0:
+            amt=amount
+            count=0
+            i=len(coins)-1
+            while i >=0:
+                print(i,amt)
+                count+=amt//coins[i]
+                amt=amt%coins[i]
+                i-=1
+                if amt==0:
+                    minCount=min(minCount,count)
+            p-=1
+            coins.insert(0,coins.pop())
+        if minCount!=1000:
+            return minCount
+        return -1
