16d60a1ce4bd5eb3a12a1422a40798a849e7fd66
ParthIll <146685408+ParthIll@users.noreply.github.com>
2026-07-07 08:34:14 -0500

    Add: coin-change - submission-21

---
+++ Data Structures & Algorithms/coin-change/submission-21.py
@@ -0,0 +1,25 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        if amount == 0:
+            return 0
+        coins.sort(reverse=True)
+        q=deque()
+        for coin in coins:
+            q.append((coin,1))
+        reached = {}
+        while q:
+            coin=q.popleft()
+            if coin[0] in reached:
+                continue
+            
+            reached[coin[0]]=1
+            
+            if amount==coin[0]:
+                return coin[1]
+            if amount-coin[0] in coins:
+                return coin[1]+1
+            else:
+                for c in coins:
+                    if c+coin[0]<amount:
+                        q.append((c+coin[0],coin[1]+1))
+        return -1
