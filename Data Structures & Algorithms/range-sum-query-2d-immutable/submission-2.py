3ee14c72e18901dcc7b9533e48b8075b9e952b2d
ParthIll <146685408+ParthIll@users.noreply.github.com>
2026-07-22 10:15:52 -0500

    Add: range-sum-query-2d-immutable - submission-2

---
+++ Data Structures & Algorithms/range-sum-query-2d-immutable/submission-2.py
@@ -0,0 +1,35 @@
+class NumMatrix:
+
+    def __init__(self, matrix: List[List[int]]):
+        self.matrix=matrix
+        ROWS=len(matrix)
+        COLS = len(matrix[0])
+        self.sums =[[0]*COLS for _ in range(ROWS)]
+        for r in range(ROWS):
+            for c in range(COLS):
+                self.sums[r][c]=matrix[r][c]
+                
+                if r-1 in range(ROWS) and c-1 in range(COLS):
+                    inner = self.sums[r-1][c-1]
+                    self.sums[r][c]+=(self.sums[r-1][c])+(self.sums[r][c-1]-inner)
+                elif r-1 in range(ROWS):
+                    self.sums[r][c]+=self.sums[r-1][c]
+                elif c-1 in range(COLS):
+                    self.sums[r][c]+=self.sums[r][c-1]
+        print(self.sums)
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        ret= self.sums[row2][col2]
+        if row1-1 in range(len(self.matrix)):
+            ret-=self.sums[row1-1][col2]
+        if col1-1 in range(len(self.matrix[0])):
+            ret-=self.sums[row2][col1-1]
+            if row1-1 in range(len(self.matrix)):
+                ret+=self.sums[row1-1][col1-1]
+        return ret
+        
+
+
+# Your NumMatrix object will be instantiated and called as such:
+# obj = NumMatrix(matrix)
+# param_1 = obj.sumRegion(row1,col1,row2,col2)
