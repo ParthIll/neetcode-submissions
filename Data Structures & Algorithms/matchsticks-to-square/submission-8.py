7837609cefb4f47da5543e7227cb2224a424afdd
ParthIll <146685408+ParthIll@users.noreply.github.com>
2026-07-30 09:54:25 -0500

    Add: matchsticks-to-square - submission-8

---
+++ Data Structures & Algorithms/matchsticks-to-square/submission-8.py
@@ -0,0 +1,23 @@
+class Solution:
+    def makesquare(self, matchsticks: List[int]) -> bool:
+        tot = sum(matchsticks)
+        print(tot)
+        if tot%4!=0:
+            return False
+        sid=tot/4
+        if max(matchsticks)>sid:
+            return False
+        def dfs(i,sides):
+            if i==len(matchsticks):
+                return True
+            for j in range(len(sides)):
+                if sides[j]+matchsticks[i]<=sid:
+                    scop=sides.copy()
+                    scop[j]+=matchsticks[i]
+                    if dfs(i+1,scop):
+                        return True
+            return False
+
+        return dfs(0,[0,0,0,0])
+        
+
