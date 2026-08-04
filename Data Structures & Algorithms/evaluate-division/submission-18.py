c617445897b4b3c605593e4ba8a4132cf627533b
ParthIll <146685408+ParthIll@users.noreply.github.com>
2026-08-04 10:09:09 -0500

    Add: evaluate-division - submission-18

---
+++ Data Structures & Algorithms/evaluate-division/submission-18.py
@@ -0,0 +1,38 @@
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        eqMap=defaultdict(list)
+        for i, (fro,to) in enumerate(equations):
+            val = values[i]
+            eqMap[fro].append((to,val))
+            eqMap[to].append((fro,1/val))
+        print(eqMap)
+        def dfs(fro,to):
+            print(fro,to,visited)
+            if fro==to:
+                if fro in eqMap and eqMap[fro]:
+                    return 1
+                else:
+                    return -1
+            minleng=-1
+            for x,val in eqMap[fro]:
+                print(x,val,visited)
+                if x not in visited:
+                    visited.add(x)
+                    if x == to:
+                        return val
+                    else:
+                        tryi = val*dfs(x,to)
+                        if tryi>0:
+                            return tryi
+                    visited.remove(x)
+                    
+            return -1
+
+        ret=[]
+        for f,t in queries:
+            visited=set()
+            visited.add(f)
+            ret.append(dfs(f,t))
+        return ret
+
+
