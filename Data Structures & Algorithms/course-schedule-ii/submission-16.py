40421bedd4ceff34cb6a513e074f141b818f1b71
ParthIll <146685408+ParthIll@users.noreply.github.com>
2026-08-07 08:33:49 -0500

    Add: course-schedule-ii - submission-16

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-16.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        ret=[]
+        rset=set()
+        preq=defaultdict(set)
+        for crs,pre in prerequisites:
+            preq[crs].add(pre)
+        while len(rset)!=numCourses:
+            f=len(rset)
+            for j in range(numCourses):
+                if j not in preq and j not in rset:
+                    rset.add(j)
+                    ret.append(j)
+                elif j in preq:
+                    if preq[j]<=rset:
+                        del preq[j]
+                        rset.add(j)
+                        ret.append(j)
+            x=len(rset)
+            if f==x:
+                return []
+        return ret
+                        
