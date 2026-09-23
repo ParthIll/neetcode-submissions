bb238f35b1b1ef36a2aaca38649838b6de765924
ParthIll <146685408+ParthIll@users.noreply.github.com>
2026-09-23 01:46:53 -0500

    Add: burst-balloons - submission-9

---
+++ Data Structures & Algorithms/burst-balloons/submission-9.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxCoins(self, nums: List[int]) -> int:
+        nums = [1]+nums+[1]
+        cache = {}
+        
+        
+        def foo(nums):
+            if tuple(nums) in cache:
+                return cache[tuple(nums)]
+            maxVal = 0
+            for i in range(1,len(nums)-1):
+                maxVal = max(maxVal,nums[i]*nums[i-1]*nums[i+1]+foo(nums[:i]+nums[i+1:]))
+            cache[tuple(nums)]=maxVal
+            return maxVal
+        
+        
+        
+        x = foo(nums)
+        return x
